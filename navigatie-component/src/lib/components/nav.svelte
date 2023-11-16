a8d2eb8f5b168c1bdddebeecb3846a4d9d6678e9
Demivdm <demi.van.der.maarl@hva.nl>
2023-11-16 19:30:00 +0100

    nav set up

---
+++ navigatie-component/src/lib/components/nav.svelte
@@ -0,0 +1,105 @@
+<script>
+  let mobileDropdownActive = false;
+  let filterDropdownActive = false;
+  function openMobileMenu() {
+    mobileDropdownActive = !mobileDropdownActive;
+
+    // filter dropdown sluit ook wanneer menu is gesloten
+    if (!mobileDropdownActive) {
+      filterDropdownActive = false;
+    }
+  }
+
+  function openFilter() {
+    filterDropdownActive = !filterDropdownActive;
+  }
+</script>
+
+<nav>
+  <section class="nav-bar" class:active={mobileDropdownActive}>
+    <!-- menu is a mobile only button -->
+    <button on:click={openMobileMenu}>Menu</button>
+    {#if mobileDropdownActive}
+      <div class="mobile-menu-dropdown">
+        <button on:click={openFilter}> Filteren en zoeken</button>
+        <a href="/">Inloggen</a>
+        <a href="/">Werkvorm uploaden</a>
+      </div>
+    {/if}
+    <section class="filter-dropdown" class:active={filterDropdownActive}>
+      {#if filterDropdownActive}
+        <section class="search-bar">
+          <input type="search" />
+          <button>Zoeken</button>
+        </section>
+        <section class="filter-menu">
+          <ul class="focus">
+            <button class="focus-button">Focus</button>
+            <div>
+              <div class="drop-down-focus">
+                <li><input type="checkbox" />Ik</li>
+                <li><input type="checkbox" />De ander</li>
+                <li><input type="checkbox" />De wereld</li>
+              </div>
+            </div>
+          </ul>
+          <ul class="doel">
+            <button class="focus-button">Doel</button>
+            <div>
+              <div class="drop-down-doel">
+                <li><input type="checkbox" />Professioneel id ontwikkelen</li>
+                <li>
+                  <input type="checkbox" />Persoonlijke interesse aanspreken
+                </li>
+                <li><input type="checkbox" />Kennis opdoen</li>
+                <li><input type="checkbox" />Richtlijnen toepassen</li>
+                <li><input type="checkbox" />Golden oldies verrijken</li>
+                <li><input type="checkbox" />Tools toepassen</li>
+                <li><input type="checkbox" />Dialoog voeren</li>
+                <li><input type="checkbox" />Praktijk betrekken</li>
+              </div>
+            </div>
+          </ul>
+          <ul class="onderwerp">
+            <button class="focus-button">Onderwerp</button>
+            <div>
+              <div class="drop-down-onderwerp">
+                <li><input type="checkbox" />Duurzaamheid</li>
+                <li><input type="checkbox" />Diversiteit</li>
+                <li><input type="checkbox" />Digitalisering</li>
+                <li><input type="checkbox" />Democratie</li>
+              </div>
+            </div>
+          </ul>
+          <ul class="fase">
+            <button class="focus-button">Fase</button>
+            <div>
+              <div class="drop-down-fase">
+                <li><input type="checkbox" />Opwarmer</li>
+                <li><input type="checkbox" />Verbreden</li>
+                <li><input type="checkbox" />Verdiepen</li>
+                <li><input type="checkbox" />Uitwisselen</li>
+              </div>
+            </div>
+          </ul>
+        </section>
+      {/if}
+    </section>
+  </section>
+</nav>
+
+<style>
+  nav {
+    font-size: 2em;
+  }
+
+  .filter-dropdown,
+  .mobile-menu-dropdown {
+    display: none;
+  }
+
+  .nav-bar.active .mobile-menu-dropdown,
+  .filter-dropdown.active {
+    display: block;
+  }
+</style>
---
+++ navigatie-component/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Header from "$lib/Components/Header.svelte";
+  import Footer from "$lib/Components/Footer.svelte";
+  import { page } from "$app/stores";
+  import Nav from "../lib/components/nav.svelte";
+</script>
+
+<Header path={$page.url.pathname} />
+
+<slot />
+<Nav />
+
+<Footer />
